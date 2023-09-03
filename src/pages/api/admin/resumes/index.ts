af01eadfdee6d3438685d6926e12d10c2dff2133
JaneIRL <103539883+JaneIRL@users.noreply.github.com>
2023-09-03 18:24:06 -0500

    Add logger to /api/admin/resumes

--- src/pages/api/admin/resumes/index.ts
+++ src/pages/api/admin/resumes/index.ts
@@ -3,43 +3,51 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import clientPromise from '@/lib/db'
 import User, { getFullName } from '@/lib/db/models/User'
+import logger from '@/lib/logger'
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
-	if (req.method !== 'GET') {
-		throw new Error(`Unsupported ${req.method}`)
-	}
-
-  // get all users with a resume by filter
-	const client = await clientPromise
-	const users: User[] = await client
-		.db()
-		.collection<User>('users')
-		.find({
-			'application.resume': { $regex: /^data:application\/pdf;base64,/ },
-		})
-		.toArray()
-
-	if (!users) {
-		throw new Error('No users found')
-	}
-
-  // create a zip file with all resumes
-	const jzip = new JSZip()
-	users.forEach((user) => {
-		const resume = user.application?.resume?.split('data:application/pdf;base64,')
-		if (resume?.length !== 2) {
-			return
+	try {
+		if (req.method !== 'GET') {
+			throw new Error(`Unsupported ${req.method}`)
 		}
-		jzip.file(`${getFullName(user)}.pdf`, resume[1], { base64: true })
-	})
 
-  // send the zip file
-	const zip = await jzip.generateAsync({ type: 'nodebuffer' })
-	res.setHeader('Content-Type', 'application/zip')
-	res.setHeader('Content-Disposition', 'attachment; filename=resumes.zip')
-  
-	return res.status(200).send(zip)
+		// get all users with a resume by filter
+		const client = await clientPromise
+		const users: User[] = await client
+			.db()
+			.collection<User>('users')
+			.find({
+				'application.resume': { $regex: /^data:application\/pdf;base64,/ },
+			})
+			.toArray()
+
+		if (!users) {
+			throw new Error('No users found')
+		}
+
+		// create a zip file with all resumes
+		const jzip = new JSZip()
+		users.forEach((user) => {
+			const resume = user.application?.resume?.split(
+				'data:application/pdf;base64,',
+			)
+			if (resume?.length !== 2) {
+				return
+			}
+			jzip.file(`${getFullName(user)}.pdf`, resume[1], { base64: true })
+		})
+
+		// send the zip file
+		const zip = await jzip.generateAsync({ type: 'nodebuffer' })
+		res.setHeader('Content-Type', 'application/zip')
+		res.setHeader('Content-Disposition', 'attachment; filename=resumes.zip')
+
+		return res.status(200).send(zip)
+	} catch (e) {
+		logger.error(e, '[/api/admin/resumes]')
+		return res.status(500)
+	}
 }
