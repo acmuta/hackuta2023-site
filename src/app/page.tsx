a63eeb822c03cbc88df7de086b6a5c98850c3287
Parth <pxs4528@mavs.uta.edu>
2023-09-12 02:56:55 -0500

    add schedule (#56)
    * add basic calendar
    
    * fix calendar
    
    * make it respionsive :biting_lip:
    
    * added better color and break-words
    
    * white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { SVGProps } from 'react-html-props'
 import { twJoin } from 'tailwind-merge'
 
 import { Box } from '@/components/Box'
 import { LinkButton } from '@/components/Button'
+import { getEvents, HackathonCalendar } from '@/components/calendar'
 import { ClippedBadge } from '@/components/ClippedBadge'
 import { HackTicket } from '@/components/HackTicket'
 import { SponsorTicket, SponsorTicketKind } from '@/components/SponsorTicket'
@@ -97,7 +99,9 @@ const CTAArrow = ({ ...props }: CTAArrowProps) => {
 }
 
 export default async function Landing() {
-	// const [events, faqs] = await Promise.all([getEvents(), getFaqs()])
+	const startDate = new Date('10/07/2023 06:00:00')
+	const endDate = new Date('10/08/2023 18:00:00')
+	const events = await getEvents()
 	const faqs = await getFaqs()
 	const sponsors = [
 		{
@@ -134,6 +138,7 @@ export default async function Landing() {
 
 	return (
 		<>
+		
 			<Box
 				direction="column"
 				alignItems="center"
@@ -183,7 +188,7 @@ export default async function Landing() {
 							<WavyPattern className="w-32" />
 						</h2>
 						<div className="font-shrimp text-white tracking-wider uppercase">
-							Coming soon...
+						<HackathonCalendar startDate={startDate} endDate={endDate} events={events}/>
 						</div>
 						<ClippedBadge
 							className="md:block hidden w-[150px] h-[150px] absolute right-[10%] rotate-[15deg] mt-[-5rem]"
--- src/lib/db/models/Event.ts
+++ src/lib/db/models/Event.ts
@@ -1,12 +1,8 @@
 import z from 'zod'
 
 export const EventSchema = z.object({
-	startTime: z.string().datetime(),
-	durationMins: z.number().positive().int(),
-	title: z.string(),
-	shortDesc: z.string(),
-	longDesc: z.string(),
-	categories: z.string().array(),
+	title 	 : z.string(),
+	date 	 : z.string().datetime()
 })
 
 export type EventModel = z.infer<typeof EventSchema>
