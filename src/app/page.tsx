83066ac9aec3175db4884f422899acc75081a5c4
Ryan Le <31494954+lryanle@users.noreply.github.com>
2023-09-21 07:44:03 -0500

    trimmed button spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -212,7 +212,7 @@ export default async function Landing() {
 							{/* Code below to allow tailwind to compile custom backgrounds (they dont load at compile time) */}
 							<div className="hidden w-0 h-0 bg-hackuta-ticket-blue bg-hackuta-ticket-red bg-hackuta-noqrcode bg-hackuta-sqrbg-unregistered bg-hackuta-sqrbg-ruby"></div>
 						</div>
-						<div className="flex gap-3">
+						<div className="flex flex-col md:flex-row justify-center items-center flex-wrap gap-1 md:gap-3">
 							<LinkButton href="/apply" className="text-2xl">
 								Apply
 							</LinkButton>
