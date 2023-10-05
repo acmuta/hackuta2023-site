8e7435fead559d4d1d6462fd5e299037c9dd8be3
Sneh Acharya <20356sneh@gmail.com>
2023-10-04 20:45:02 -0500

    redirect to dashboard

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,29 +1,31 @@
-// export default async function Home() {
-// 	const { user } = getEnhancedSession(headers())
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-// 	if (!user) {
-// 		redirect('/api/auth/signin?callbackUrl=%2Fapply')
-// 	} else if (user.application) {
-// 		redirect('/dashboard')
-// 	}
+import { getEnhancedSession } from '@/lib/utils/server'
 
-// 	return <ApplicationForm />
-// }
-function ApplicationClosedMessage() {
-	return (
-		<div>
-			<p>
-				Thank you for your interest in joining us at HackUTA 2023.
-				Unfortunately, we&apos;ve hit our registration capacity and can no
-				longer accept applications.
-			</p>
-			<p>
-				We will have <strong>walk-in registrations</strong>{' '}
-				starting at 11:00am on Saturday, October 7th as space becomes
-				available.
-			</p>
-		</div>
-	)
+export default async function Home() {
+	const { user } = getEnhancedSession(headers())
+
+	// If the user isn't logged in, show the registration closed message
+	if (!user) {
+		return (
+			<div>
+				<p>
+					Thank you for your interest in joining us at HackUTA 2023.
+					Unfortunately, we&apos;ve hit our registration capacity and can
+					no longer accept applications.
+				</p>
+				<p>
+					We will have <strong>walk-in registrations</strong>{' '}
+					starting at 11:00am on Saturday, October 7th as space becomes
+					available.
+				</p>
+			</div>
+		)
+	}
+
+	// If the user has already submitted an application, redirect them to the dashboard
+	if (user.application) {
+		redirect('/dashboard')
+	}
 }
-
-export default ApplicationClosedMessage
