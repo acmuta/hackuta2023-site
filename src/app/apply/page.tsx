dcfd69eac8b472444807613fd25eb52dfe06582b
Sneh Acharya <20356sneh@gmail.com>
2023-10-04 20:32:44 -0500

    Application limit reached message

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,18 +1,29 @@
-import { headers } from 'next/headers'
-import { redirect } from 'next/navigation'
+// export default async function Home() {
+// 	const { user } = getEnhancedSession(headers())
 
-import { getEnhancedSession } from '@/lib/utils/server'
+// 	if (!user) {
+// 		redirect('/api/auth/signin?callbackUrl=%2Fapply')
+// 	} else if (user.application) {
+// 		redirect('/dashboard')
+// 	}
 
-import { ApplicationForm } from './ApplicationForm'
-
-export default async function Home() {
-	const { user } = getEnhancedSession(headers())
-
-	if (!user) {
-		redirect('/api/auth/signin?callbackUrl=%2Fapply')
-	} else if (user.application) {
-		redirect('/dashboard')
-	}
-
-	return <ApplicationForm />
+// 	return <ApplicationForm />
+// }
+function ApplicationClosedMessage() {
+	return (
+		<div>
+			<p>
+				Thank you for your interest in joining us at HackUTA 2023.
+				Unfortunately, we&apos;ve hit our registration capacity and can no
+				longer accept applications.
+			</p>
+			<p>
+				We will have <strong>walk-in registrations</strong>{' '}
+				starting at 11:00am on Saturday, October 7th as space becomes
+				available.
+			</p>
+		</div>
+	)
 }
+
+export default ApplicationClosedMessage
