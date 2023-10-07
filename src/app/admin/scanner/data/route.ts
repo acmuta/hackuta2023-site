0012ec08a5419759b9d4fc43e7b61f35efb0254d
JaneIRL <103539883+JaneIRL@users.noreply.github.com>
2023-10-07 02:02:06 -0500

    Add Shop tab to Scanner

---
+++ src/app/admin/scanner/data/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+
+import clientPromise from '@/lib/db'
+import Event from '@/lib/db/models/Event'
+import { ShopSwag, ShopSwagCollection } from '@/lib/db/models/ShopSwap'
+import logger from '@/lib/logger'
+
+export const dynamic = 'force-dynamic'
+
+export interface ScannerDataResponse {
+	events: Event[]
+	swags: ShopSwag[]
+}
+
+export async function GET() {
+	try {
+		const client = await clientPromise
+		const events = await client
+			.db()
+			.collection<Event>('events')
+			.find({})
+			.toArray()
+		const swags = await client
+			.db()
+			.collection<ShopSwag>(ShopSwagCollection)
+			.find({})
+			.toArray()
+		return NextResponse.json({ events, swags } satisfies ScannerDataResponse)
+	} catch (e) {
+		logger.error(e, '[/admin/scanner/data]')
+		return NextResponse.json(
+			{
+				events: [],
+				swags: [],
+			} satisfies ScannerDataResponse,
+		)
+	}
+}
