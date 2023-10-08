3e22a49672aa86081772c92046465b25ec9f4092
JaneIRL <103539883+JaneIRL@users.noreply.github.com>
2023-10-08 10:17:53 -0500

    Fix leaderboard (#78)
    Co-authored-by: Parth <parthsharma.cs@gmail.com>

--- src/app/dashboard/score.tsx
+++ src/app/dashboard/score.tsx
@@ -1,27 +1,21 @@
 import clientPromise from '@/lib/db'
-import { Role } from '@/lib/db/models/Role'
-import User from '@/lib/db/models/User'
-import { getEnhancedSession } from '@/lib/utils/server'
-import { headers } from 'next/headers'
+import User, { getFullName } from '@/lib/db/models/User'
 import UserDataTable from './info/points'
 
 export default async function Users() {
-	const { perms } = getEnhancedSession(headers())
 	const client = await clientPromise
-	const roles = await client.db()
-		.collection<Role>('roles')
-		.find({}, { projection: { granted: 0 } })
-		.toArray()
-	const users = await client.db()
+	const users = (await client.db()
 		.collection<User>('users')
-		.find({}, { projection: { 'application.resume': 0 } })
-		.toArray()
+		.find({ pointsObtained: { $gt: 0 } }, {
+			projection: { 'application.resume': 0 },
+		})
+		.sort({ pointsObtained: 'descending' })
+		.toArray())
+		.map((u) => ({ name: getFullName(u), pointsObtained: u.pointsObtained! }))
 	return (
 		<>
 			<UserDataTable
-				allRoles={roles.map((r) => r._id)}
-				users={JSON.parse(JSON.stringify(users))}
-				perms={perms}
+				users={users}
 			/>
 			<br />
 		</>
